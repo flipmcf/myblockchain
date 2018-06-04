aced850899d23b13e8d5e1614737cb37ebc6f294
flipmcf <flipmcf@gmail.com>
2018-06-04 10:45:35 -0400

    Up to 'creating new blocks'

---
+++ blockchain.py
@@ -0,0 +1,27 @@
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        
+    def new_block(self):
+        pass
+    
+    def new_transactions(self, sender, recipient, amount):
+        self.current_transactions.append({'sender': sender,
+                                          'recipient': recipient,
+                                          'amount': amount,
+                                          })
+        
+        return self.last_block['index'] + 1
+    
+    @staticmethod
+    def hash(block):
+        pass
+    
+    @property
+    def last_block(self):
+        pass
+    
+    
+        
